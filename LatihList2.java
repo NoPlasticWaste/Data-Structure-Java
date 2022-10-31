011e8439b40ff4bdce84e047d7e0ae3fbd7c9581
Vivien <endarlan11@gmail.com>
2022-10-31 17:54:41 +0700

    Contoh array list

---
+++ LatihList2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class LatihList2 {
+    public static void main (String[] args) {
+        List Q = new ArrayList();
+        Q.add("D");
+        Q.add("O");
+        Q.add("N");
+        Q.add("E");
+        System.out.println("List Java");
+        for (Object queueList : Q) {
+            System.out.println(queueList+"->");
+        }
+    }
+}
